b0199c1fb0cc220fc4090ab687f20128197b6e4a
廖再润 <lzr3@us.meitu.com>
2023-10-13 13:59:13 +0800

    多语言文件写入模式新增append类型

--- lib/LocalizationPlatform.rb
+++ lib/LocalizationPlatform.rb
@@ -8,10 +8,6 @@ module Pixab
 
   class LocalizationPlatform
 
-    def initialize()
-      @file_mode = 'w+'
-    end
-
     def run(localized_info_category, commands)
       resolve_commands(commands)
     end
@@ -24,10 +20,7 @@ module Pixab
         command = commands[index]
         case command
         when "--mode"
-          mode = commands[index + 1]
-          if mode == 'add'
-            @file_mode = 'a+'
-          end
+          @mode = commands[index + 1]
         end
       end
     end
@@ -35,6 +28,15 @@ module Pixab
     def dir_name(locale)
       return ''
     end 
+
+    def file_mode
+      case @mode
+      when 'add'
+        return 'a+'
+      else 
+        return 'w+'
+      end
+    end
 
   end
 
@@ -50,40 +52,83 @@ module Pixab
     def run(localized_info_category, commands)
       super(localized_info_category, commands)
 
-      begin_prompt = "\n// Created from phrase api <<<<<<<<<< begin\n"
-      end_prompt = "\n// Created from phrase api <<<<<<<<<< end\n"
-
       localized_info_category.each do |locale, localized_infos|
         content_dir_path = dir_name(locale)
         if !Utilities.dir_exist(content_dir_path)
           FileUtils.mkdir_p content_dir_path
         end
         content_file_path = "#{content_dir_path}/#{File_name}"
-        if @file_mode == 'a+' and File::exists?(content_file_path)
-          # 移除以前脚本添加的本地化文案
-          content = File.read(content_file_path)
-          content = content.sub(/#{begin_prompt}.*#{end_prompt}/m, '')
-          File.open(content_file_path, 'w+') do |file|
-            file.syswrite(content)
-          end
-        end
-        File.open(content_file_path, @file_mode) do |file|
-          file.syswrite(begin_prompt)
-          localized_infos.each do |localized_info|
-            value = localized_info['value'].gsub(/["]/, '\\\\\0')
-            value = value.gsub(/%s/, '%@')
-            file.syswrite("\n\"#{localized_info['key']}\" = \"#{value}\";\n")
-          end
-          file.syswrite(end_prompt)
+        
+        case @mode
+        when 'add'
+          write_file_add(content_file_path, localized_infos)
+        when 'replace'
+          write_file_replace(content_file_path, localized_infos)
+        when 'append'
+          write_file_append(content_file_path, localized_infos)
+        else 
+          write_file_by_mode(content_file_path, localized_infos, 'w')
         end
       end
     end
-
 
     def dir_name(locale)
       "#{locale}.lproj"
     end
 
+    def write_file_add(content_file_path, localized_infos)
+      if File::exists?(content_file_path)
+        # 移除以前脚本添加的本地化文案
+        content = File.read(content_file_path)
+        content = content.sub(/#{begin_prompt}.*#{end_prompt}/m, '')
+        File.open(content_file_path, 'w+') do |file|
+          file.syswrite(content)
+        end
+      end
+      write_file_by_mode(content_file_path, localized_infos, file_mode)
+    end
+
+    def write_file_replace(content_file_path, localized_infos)
+      # write_file_by_mode(content_file_path, localized_infos, file_mode)
+    end
+
+    def write_file_append(content_file_path, localized_infos)
+      if File::exists?(content_file_path)
+        file_content = File.read(content_file_path)
+        if index = file_content.index(end_prompt)
+          localized_infos.each do |localized_info|
+            file_content.insert(index, genetate_localized_string(localized_info))        
+          end
+          File.open(content_file_path, 'w') { |file| file.write(file_content) }
+          return
+        end
+      end
+      write_file_by_mode(content_file_path, localized_infos, 'a+')
+    end
+
+    def write_file_by_mode(content_file_path, localized_infos, mode)
+      File.open(content_file_path, mode) do |file|
+        file.syswrite(begin_prompt)
+        localized_infos.each do |localized_info|
+          file.syswrite(genetate_localized_string(localized_info))
+        end
+        file.syswrite(end_prompt)
+      end
+    end
+
+    def begin_prompt
+      return "\n// Created from phrase api <<<<<<<<<< begin\n"
+    end
+
+    def end_prompt
+      return "\n// Created from phrase api <<<<<<<<<< end\n"
+    end
+
+    def genetate_localized_string(localized_info)
+      value = localized_info['value'].gsub(/["]/, '\\\\\0')
+      value = value.gsub(/%s/, '%@')
+      return "\n\"#{localized_info['key']}\" = \"#{value}\";\n"
+    end
 
   end
 
@@ -115,7 +160,7 @@ module Pixab
           FileUtils.mkdir_p content_dir_path
         end
         content_file_path = "#{content_dir_path}/#{File_name}"
-        File.open(content_file_path, @file_mode) do |file|
+        File.open(content_file_path, file_mode) do |file|
           file.syswrite("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
           file.syswrite("<resources>\n")
           localized_infos.each do |localized_info|
